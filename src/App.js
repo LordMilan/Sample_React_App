c5ec6fe1a755277a54564cbf2b323782bcf58bdd
LordMilan <milan.mahat.739@gmail.com>
2024-08-29 22:09:45 +0545

    Added react code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Nav from './Components/Nav/Nav';
+import Counter from './Components/Counter/Counter';
+import { useState } from 'react';
+
+function App() {
+
+  let total = 0;
+  const [arr, setArr] = useState([
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 }]);
 
 
-class App extends Component {
-  render() {
-    console.log("Host URL"+process.env.PUBLIC_URL);
-    return (
-
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
-    </Router>
-    );
+  const handleDelete = (deleteId) => {
+    const deleteArr = arr.filter(({ id }) => id !== deleteId);
+    setArr(deleteArr);
   }
+
+  const incrementHandle = (count) => {
+
+    const newArr = [...arr];
+    const index = newArr.indexOf(count);
+    newArr[index] = { ...count };
+    newArr[index].value++;
+    setArr(newArr);
+
+  }
+
+  arr.map(({ value }) => total += value);
+
+
+
+
+
+  return (
+
+
+    <div className="App_content">
+      <Nav total={total}></Nav>
+      <Counter arr={arr} handleDelete={handleDelete} incrementHandle={incrementHandle}></Counter>
+    </div>
+
+
+  );
 }
 
 export default App;
